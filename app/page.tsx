84acad6d39d1b2268cb4ab05daf438cdee7d5c15
brunozanao <brunovinicius_2012@hotmail.com>
2024-07-09 15:26:04 -0300

    commit 5

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className='flex h-screen flex-col items-center justify-between'>
       <div className="m-auto items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32">
-        <h1 className="text-2xl font-bold">Carbon.Std
+        <h1 className="text-2xl font-bold">theblockchain.eth || boilerplate
         </h1>
         <p className="px-8 mt-8 mb-12 text-lg">NextJS 14.x, NextAuth, SIWE, Rainbowkit, DrizzleORM, wagmi, tanstack, TailwindCSS, DaisyUI</p>
         <div
